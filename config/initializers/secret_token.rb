3ed2d248b16473dacb9ff4cfcd8d1afcd545a23b
bacancy119 <bacancy119@bacancy119-desktop.(none)>
2013-01-18 12:43:19 +0530

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BestPlaceDemo::Application.config.secret_token = '7c867337de77ca9f3680644f23ecafe326951243d6ad29b6bbf95cefb7ae07577cf348b70c6cafe6a7b0375466ac1fd6d124331d5fe32e3f97db5675de05eda0'
